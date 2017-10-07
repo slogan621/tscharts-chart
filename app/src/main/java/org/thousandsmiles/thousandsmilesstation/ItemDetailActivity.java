a6a8cb273a446f3808c9fcf6c2757e87743e00a6
Syd Logan <slogan621@gmail.com>
2017-10-07 11:07:40 -0700

    item detail refactor part 2 (of 2)

--- app/src/main/java/org/thousandsmiles/thousandsmilesstation/ItemDetailActivity.java
+++ app/src/main/java/org/thousandsmiles/thousandsmilesstation/ItemDetailActivity.java
@@ -17,13 +17,10 @@ package org.thousandsmiles.thousandsmilesstation;
  * limitations under the License.
  */
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -50,7 +47,7 @@ public class ItemDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_list);
+        setContentView(R.layout.activity_item_detail);
 
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
--- app/src/main/java/org/thousandsmiles/thousandsmilesstation/StationActivity.java
+++ app/src/main/java/org/thousandsmiles/thousandsmilesstation/StationActivity.java
@@ -17,14 +17,10 @@
 
 package org.thousandsmiles.thousandsmilesstation;
 
-import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -32,10 +28,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -86,7 +80,7 @@ public class StationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_list);
+        setContentView(R.layout.activity_item_detail);
     }
 
     private class UpdatePatientLists extends AsyncTask<Object, Object, Object> {
