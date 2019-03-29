a87899e1a73e442cf1c1c1605981d73b4b3dba35
Syd Logan <slogan621@gmail.com>
2019-03-29 00:05:59 -0700

    implement xray image map editor

--- app/src/main/java/org/thousandsmiles/thousandsmilesstation/AppPatientXRayEditorFragment.java
+++ app/src/main/java/org/thousandsmiles/thousandsmilesstation/AppPatientXRayEditorFragment.java
@@ -20,6 +20,10 @@ package org.thousandsmiles.thousandsmilesstation;
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -39,6 +43,9 @@ import android.widget.Toast;
 import org.thousandsmiles.tscharts_lib.XRay;
 import org.thousandsmiles.tscharts_lib.XRayREST;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class AppPatientXRayEditorFragment extends Fragment {
     private StationActivity m_activity = null;
     private SessionSingleton m_sess = SessionSingleton.getInstance();
@@ -46,6 +53,40 @@ public class AppPatientXRayEditorFragment extends Fragment {
     private boolean m_dirty = false;
     private View m_view = null;
     private boolean m_leaving = false;
+    private ImageMap m_childImageMap;
+    private ImageMap m_adultImageMap;
+    private ToothMapState m_childToothMapState;
+    private ToothMapState m_adultToothMapState;
+    private XRay.XRayMouthType m_mouthType;
+
+    private void initImageMaps() {
+        m_childImageMap = new ImageMap();
+        m_adultImageMap = new ImageMap();
+
+        m_childToothMapState = new ToothMapState();
+        m_adultToothMapState = new ToothMapState();
+
+        m_childImageMap.setColoredPixel(getResources().getColor(R.color.colorThousandsmiles));
+        m_childImageMap.setUncoloredPixel(getResources().getColor(R.color.xrayGray));
+        BitmapDrawable b = (BitmapDrawable)(this.getResources().getDrawable(R.drawable.child_teeth_gray));
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(b.getBitmap(), 577, 714, false);
+        m_childImageMap.setWidth(577);
+        m_childImageMap.setHeight(714);
+        m_childImageMap.setBitmap(resizedBitmap);
+
+        m_adultImageMap.setColoredPixel(getResources().getColor(R.color.colorThousandsmiles));
+        m_adultImageMap.setUncoloredPixel(getResources().getColor(R.color.xrayGray));
+        b = (BitmapDrawable) this.getResources().getDrawable(R.drawable.adult_teeth_gray);
+        resizedBitmap = Bitmap.createScaledBitmap(b.getBitmap(), 587, 877, false);
+        m_adultImageMap.setWidth(587);
+        m_adultImageMap.setHeight(877);
+        m_adultImageMap.setBitmap(resizedBitmap);
+
+        Point p = new Point(237, 24);
+        m_adultImageMap.addImageMapObject(p, 50, 40, 1);
+        p = new Point(301, 21);
+        m_adultImageMap.addImageMapObject(p, 50, 40, 2);
+    }
 
     public static AppPatientXRayEditorFragment newInstance() {
         return new AppPatientXRayEditorFragment();
@@ -64,7 +105,6 @@ public class AppPatientXRayEditorFragment extends Fragment {
     {
         RadioButton rb1, rb2;
         XRay.XRayType type;
-        XRay.XRayMouthType mouthType;
 
         if (m_xray != null) {
             rb1 = (RadioButton) m_view.findViewById(R.id.xray_type_full);
@@ -83,8 +123,8 @@ public class AppPatientXRayEditorFragment extends Fragment {
 
             rb1 = (RadioButton) m_view.findViewById(R.id.xray_mouth_type_child);
             rb2 = (RadioButton) m_view.findViewById(R.id.xray_mouth_type_adult);
-            mouthType = m_xray.getMouthType();
-            switch(mouthType) {
+            m_mouthType = m_xray.getMouthType();
+            switch(m_mouthType) {
                 case XRAY_MOUTH_TYPE_CHILD:
                     rb1.setChecked(true);
                     rb2.setChecked(false);
@@ -94,7 +134,7 @@ public class AppPatientXRayEditorFragment extends Fragment {
                     rb2.setChecked(true);
                     break;
             }
-            setMouthTypeImage(mouthType);
+            setMouthTypeImage();
         }
     }
 
@@ -128,14 +168,74 @@ public class AppPatientXRayEditorFragment extends Fragment {
         m_dirty = false;
     }
 
-    private void setMouthTypeImage(XRay.XRayMouthType mouthType)
+    private void setMouthTypeImage()
     {
         ImageView img = (ImageView) m_view.findViewById(R.id.xray_mouth_image);
-        if (mouthType == XRay.XRayMouthType.XRAY_MOUTH_TYPE_CHILD) {
-            img.setImageResource(R.drawable.child_teeth);
+
+        if (m_mouthType == XRay.XRayMouthType.XRAY_MOUTH_TYPE_CHILD) {
+            img.setImageBitmap(m_childImageMap.getBitmap());
         } else {
-            img.setImageResource(R.drawable.adult_teeth);
+            img.setImageBitmap(m_adultImageMap.getBitmap());
         }
+
+        colorTeeth();
+    }
+
+    private void colorTeeth() {
+        Iterator it;
+        ToothMapState state;
+
+        if (m_mouthType == XRay.XRayMouthType.XRAY_MOUTH_TYPE_CHILD) {
+            state = m_childToothMapState;
+            state.set(m_xray.getTeeth());
+            ArrayList<ImageMap.ImageMapObject> o = m_childImageMap.getImageMapObjects();
+            it = o.iterator();
+        } else {
+            state = m_adultToothMapState;
+            state.set(m_xray.getTeeth());
+            ArrayList<ImageMap.ImageMapObject> o = m_adultImageMap.getImageMapObjects();
+            it = o.iterator();
+        }
+
+        FloodFill ff = new FloodFill();
+        if (m_mouthType == XRay.XRayMouthType.XRAY_MOUTH_TYPE_CHILD) {
+            ff.setBitmap(m_childImageMap.getBitmap());
+            ff.setDrawableWidth(m_childImageMap.getWidth());
+            ff.setDrawableHeight(m_childImageMap.getHeight());
+        } else {
+            ff.setBitmap(m_adultImageMap.getBitmap());
+            ff.setDrawableWidth(m_adultImageMap.getWidth());
+            ff.setDrawableHeight(m_adultImageMap.getHeight());
+        }
+        while (it.hasNext()) {
+            ImageMap.ImageMapObject o = (ImageMap.ImageMapObject) it.next();
+            Point p = o.getOrigin();
+            Point q = new Point(p.x + 10, p.y + 10);
+
+            ff.setPoint(q);
+            if (state.isSelected((int) o.getTag())) {
+                if (m_mouthType == XRay.XRayMouthType.XRAY_MOUTH_TYPE_CHILD) {
+                    ff.setTargetColor(m_childImageMap.getUncoloredPixel());
+                    ff.setReplacementColor(m_childImageMap.getColoredPixel());
+                } else {
+                    ff.setTargetColor(m_adultImageMap.getUncoloredPixel());
+                    ff.setReplacementColor(m_adultImageMap.getColoredPixel());
+                }
+            } else {
+                if (m_mouthType == XRay.XRayMouthType.XRAY_MOUTH_TYPE_CHILD) {
+                    ff.setTargetColor(m_childImageMap.getColoredPixel());
+                    ff.setReplacementColor(m_childImageMap.getUncoloredPixel());
+                } else {
+                    ff.setTargetColor(m_adultImageMap.getColoredPixel());
+                    ff.setReplacementColor(m_adultImageMap.getUncoloredPixel());
+                }
+            }
+            ff.fill();
+        }
+
+        Bitmap b = ff.getBitmap();
+        ImageView img = (ImageView) m_view.findViewById(R.id.xray_mouth_image);
+        img.setImageBitmap(b);
     }
 
     private void setViewDirtyListeners()
@@ -146,8 +246,9 @@ public class AppPatientXRayEditorFragment extends Fragment {
         if (rb != null) {
             rb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    setMouthTypeImage(isChecked == true ? XRay.XRayMouthType.XRAY_MOUTH_TYPE_CHILD : XRay.XRayMouthType.XRAY_MOUTH_TYPE_ADULT);
+                    m_mouthType = isChecked == true ? XRay.XRayMouthType.XRAY_MOUTH_TYPE_CHILD : XRay.XRayMouthType.XRAY_MOUTH_TYPE_ADULT;
                     setDirty();
+                    setMouthTypeImage();
                 }
             });
         }
@@ -156,8 +257,9 @@ public class AppPatientXRayEditorFragment extends Fragment {
         if (rb != null) {
             rb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    setMouthTypeImage(isChecked == true ? XRay.XRayMouthType.XRAY_MOUTH_TYPE_ADULT : XRay.XRayMouthType.XRAY_MOUTH_TYPE_CHILD);
+                    m_mouthType = isChecked == true ? XRay.XRayMouthType.XRAY_MOUTH_TYPE_ADULT : XRay.XRayMouthType.XRAY_MOUTH_TYPE_CHILD;
                     setDirty();
+                    setMouthTypeImage();
                 }
             });
         }
@@ -280,6 +382,7 @@ public class AppPatientXRayEditorFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(false);
+        initImageMaps();
         Bundle bundle = this.getArguments();
         try {
             m_xray = (XRay) bundle.getSerializable("xray");
@@ -303,12 +406,44 @@ public class AppPatientXRayEditorFragment extends Fragment {
         mouthImage.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent ev) {
-                if (view == mouthImage) {
-                    // check to see if it intersects a tooth and respond if so.
-                    return true;
-                } else {
-                    return false;
+                boolean ret = false;
+
+                int x = (int) ev.getX();
+                int y = (int) ev.getY();
+                if (view == mouthImage && ev.getAction() == MotionEvent.ACTION_DOWN) {
+                    ImageMap.ImageMapObject im;
+                    if (m_mouthType == XRay.XRayMouthType.XRAY_MOUTH_TYPE_CHILD) {
+                        im = m_childImageMap.hitTest(x, y);
+                        if (im != null) {
+                            int tooth = (int) im.getTag();
+                            if (m_childToothMapState.isSelected(tooth)) {
+                                m_childToothMapState.clearSelected(tooth);
+                            } else {
+                                m_childToothMapState.addSelected(tooth);
+                            }
+                            m_xray.setTeeth(m_childToothMapState.getSelected());
+                            setDirty();
+                            colorTeeth();
+                        }
+                    } else {
+                        im = m_adultImageMap.hitTest(x, y);
+                        if (im != null) {
+                            int tooth = (int) im.getTag();
+                            if (m_adultToothMapState.isSelected(tooth)) {
+                                m_adultToothMapState.clearSelected(tooth);
+                            } else {
+                                m_adultToothMapState.addSelected(tooth);
+                            }
+                            m_xray.setTeeth(m_adultToothMapState.getSelected());
+                            setDirty();
+                            colorTeeth();
+                        }
+                    }
+                    if (im != null) {
+                        ret = true;
+                    }
                 }
+                return ret;
             }
         });
     }
---
+++ app/src/main/java/org/thousandsmiles/thousandsmilesstation/ImageMap.java
@@ -0,0 +1,173 @@
+/*
+ * (C) Copyright Syd Logan 2019
+ * (C) Copyright Thousand Smiles Foundation 2019
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.thousandsmiles.thousandsmilesstation;
+
+import android.graphics.Bitmap;
+import android.graphics.Point;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ImageMap {
+    public class ImageMapObject {
+        Point m_origin;
+        int m_width;
+        int m_height;
+        Object m_tag = null;
+
+        public void setOrigin(Point val) {
+            m_origin = val;
+        }
+
+        public Point getOrigin()
+        {
+            return m_origin;
+        }
+
+        public void setWidth(int val) {
+            m_width = val;
+        }
+
+        public int getWidth() {
+            return m_width;
+        }
+
+        public void setHeight(int val) {
+            m_height = val;
+        }
+
+        public int getHeight() {
+            return m_height;
+        }
+
+        public void setTag(Object o) {
+            m_tag = o;
+        }
+
+        public Object getTag() {
+            return m_tag;            // for example tooth number
+        }
+
+        public boolean hitTest(int x, int y) {
+            boolean ret  = false;
+            if (x >= m_origin.x && x <= m_origin.x + m_width) {
+                if (y >= m_origin.y && y <= m_origin.y + m_height) {
+                    ret = true;
+                }
+            }
+            return ret;
+        }
+    }
+
+    private Bitmap m_bitmap;              // the original bitmap
+    private int m_width;                  // its width
+    private int m_height;                 // its height
+
+    private int m_coloredPixel;
+    private int m_uncoloredPixel;
+    private ArrayList<ImageMapObject> m_objectList = new ArrayList<ImageMapObject>();
+    private int m_scaleFactor = 100;
+
+    enum ImageMapState {
+        COLORED,
+        UNCOLORED
+    }
+
+    public void setBitmap(Bitmap b) {
+        m_bitmap = b;
+    }
+
+    public Bitmap getBitmap() {
+        return m_bitmap;
+    }
+
+    /* set the size as it might be shown in GIMP or Photoshop, not as reported by Android */
+
+     public void setWidth(int width) {
+        m_width = width;
+    }
+
+    public void setHeight(int height) {
+        m_height = height;
+    }
+
+    public int getWidth() {
+        return m_width;
+    }
+
+    public int getHeight() {
+        return m_height ;
+    }
+
+    public void setColoredPixel(int pixel)
+    {
+        m_coloredPixel = pixel;
+    }
+
+    public void setUncoloredPixel(int pixel)
+    {
+        m_uncoloredPixel = pixel;
+    }
+
+    public int getColoredPixel()
+    {
+        return m_coloredPixel;
+    }
+
+    public int getUncoloredPixel()
+    {
+        return m_uncoloredPixel;
+    }
+
+    private ImageMapObject findImageMapObjectAt(int x, int y) {
+        ImageMapObject ret = null;
+        Iterator i = m_objectList.iterator();
+
+        while (i.hasNext()) {
+            ImageMapObject t = (ImageMapObject) i.next();
+            if (t.hitTest(x, y)) {
+                ret = t;
+                break;
+            }
+        }
+        return ret;
+    }
+
+
+    public ImageMapObject hitTest(int x, int y) {
+        ImageMapObject ret = null;
+
+        Point retPoint = new Point(x, y);
+        ret = findImageMapObjectAt(retPoint.x, retPoint.y);
+
+        return ret;
+    }
+
+    public ArrayList<ImageMapObject> getImageMapObjects() {
+        return m_objectList;
+    }
+
+    public void addImageMapObject(Point origin, int width, int height, Object tag) {
+        ImageMapObject m = new ImageMapObject();
+        m.setOrigin(origin);
+        m.setWidth(width);
+        m.setHeight(height);
+        m.setTag(tag);
+        m_objectList.add(m);
+    }
+}
